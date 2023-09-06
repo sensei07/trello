060354f1347e946f20ae55b96fc20e5866624007
sensei07 <atomkiv17@gmail.com>
2023-09-06 19:17:36 +0300

    Boards CRUD

---
+++ src/app/api/boards/[id]/route.ts
@@ -0,0 +1,66 @@
+import { prisma } from '@/core/prisma'
+import { NextResponse } from 'next/server'
+import { updateBoardDto } from '../dto'
+
+interface BoardRouteContext {
+	params: {
+		id: string
+	}
+}
+
+export async function PUT(req: Request, { params }: BoardRouteContext) {
+	const { id } = params
+	const bodyRow = await req.json()
+	const validateBody = updateBoardDto.safeParse(bodyRow)
+
+	if (!validateBody.success) {
+		return NextResponse.json(validateBody.error.issues, { status: 400 })
+	}
+
+	const findBoard = await prisma.boards.findUnique({
+		where: {
+			id,
+		},
+	})
+
+	if (!findBoard) {
+		return NextResponse.json({
+			code: 'not_found',
+			messages: 'Board not found',
+		})
+	}
+
+	const updatedBoard = await prisma.boards.update({
+		where: {
+			id,
+		},
+		data: validateBody.data,
+	})
+
+	return NextResponse.json(updatedBoard)
+}
+
+export async function DELETE(req: Request, { params }: BoardRouteContext) {
+	const { id } = params
+
+	const findBoard = await prisma.boards.findUnique({
+		where: {
+			id,
+		},
+	})
+
+	if (!findBoard) {
+		return NextResponse.json({
+			code: 'not_found',
+			messages: 'Board not found',
+		})
+	}
+
+	await prisma.boards.delete({
+		where: {
+			id,
+		},
+	})
+
+	return NextResponse.json({}, { status: 200 })
+}
---
+++ src/app/api/boards/route.ts
@@ -0,0 +1,24 @@
+import { prisma } from '@/core/prisma'
+import { NextResponse } from 'next/server'
+import { createBoardDto } from './dto'
+
+export async function GET(req: Request) {
+	const boards = await prisma.boards.findMany()
+	return NextResponse.json(boards)
+}
+
+export async function POST(req: Request) {
+	const bodyRaw = await req.json()
+	const validateBody = createBoardDto.safeParse(bodyRaw)
+
+	if (!validateBody.success) {
+		return NextResponse.json(validateBody.error.issues, { status: 400 })
+	}
+	const { title } = validateBody.data
+	const newBoard = await prisma.boards.create({
+		data: {
+			title,
+		},
+	})
+	return NextResponse.json(newBoard)
+}
