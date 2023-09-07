e8285e35f3cc82f4bafbec3ea409986baac2e3ed
sensei07 <atomkiv17@gmail.com>
2023-09-07 19:13:44 +0300

    Create boards list component with initial data

---
+++ src/components/boards-list.components.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { useBoards } from '@/hooks/use-boards'
+import { Boards } from '@prisma/client'
+import { BoardCard } from '.'
+
+interface BoardsList {
+	initialData: Boards[]
+}
+
+export function BoardsList({ initialData }: BoardsList) {
+	const { data: boards } = useBoards({ initialData })
+
+	return (
+		<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
+			{boards.map(board => (
+				<BoardCard key={board.id} id={board.id} title={board.title} />
+			))}
+		</div>
+	)
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from './board-card.component'
+export * from './boards-list.components'
 export * from './navbar.components'
 export * from './user-dropdown.component'
