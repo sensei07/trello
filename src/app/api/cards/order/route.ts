ee0eb15143b2d309a1e6a18769d7e32986479a45
sensei07 <atomkiv17@gmail.com>
2023-09-07 12:03:45 +0300

    Cards CRUD

---
+++ src/app/api/cards/order/route.ts
@@ -0,0 +1,26 @@
+import { prisma } from '@/core/prisma'
+import { NextResponse } from 'next/server'
+import { updateCardsOrderDto } from '../dto'
+
+export async function PUT(req: Request) {
+	const bodyRow = await req.json()
+	const validateBody = updateCardsOrderDto.safeParse(bodyRow)
+
+	if (!validateBody.success) {
+		return NextResponse.json(validateBody.error.issues, { status: 400 })
+	}
+
+	const queries = validateBody.data.map(({ id, order }) =>
+		prisma.cards.update({
+			where: {
+				id,
+			},
+			data: {
+				order,
+			},
+		})
+	)
+	await prisma.$transaction(queries)
+
+	return NextResponse.json({}, { status: 200 })
+}
